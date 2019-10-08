0eb7fd26e467b2d6641184fd20f4dc192102998c
Jianyu Li <jianyucode@gmail.com>
2019-10-08 14:13:28 -0600

    finsih color project

---
+++ src/BoxContainer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Box from './Box'
+import './BoxContainer.css';
+
+class BoxContainer extends Component {
+  static defaultProps = {numBoxes: 16}
+  render(){
+    const boxes = Array.from({length: this.props.numBoxes}).map(() => (<Box colors={this.props.allColors} />));
+    return(
+      <div className="BoxContainer">{boxes}</div>
+    )
+  }
+}
+
+export default BoxContainer;
